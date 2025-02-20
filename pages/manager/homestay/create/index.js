0f821e2c98d51e86f61ab7e6886a844fe73900bf
CarsonDev1 <buitritinht@gmail.com>
2025-02-20 17:14:44 +0700

    fix deploy

--- pages/manager/homestay/create/index.js
+++ pages/manager/homestay/create/index.js
@@ -61,7 +61,7 @@ const CreateHomeStay = () => {
 			});
 		},
 		onError: () => {
-			toast.error('Failed to create homestay. Please try agains.');
+			toast.error('Failed to create homestay. Please try again.');
 		},
 	});
 
